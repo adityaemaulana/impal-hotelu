7a147a033751d567517f1b781c8487a1257dc865
yusupp <hadianyusuf@gmail.com>
2018-10-09 02:50:32 +0700

    Home
    menambahkan file home

---
+++ application/views/home/home.php
@@ -0,0 +1,159 @@
+<?php
+$this->load->view('header');
+?>
+	<body>
+		<?php
+		$this->load->view('navbar');
+		?>
+		
+		<!--Image Carousel-->
+		
+	    		
+    			<img class="banner-images" src="<?php echo base_url(); ?>assets/images/banner-image-1.jpg" >
+    			
+    			
+  			
+  			<!--a class="carousel-control-prev" href="#banner-images" data-slide="prev">
+	    		<span class="carousel-image-prev"></span>
+  			</a>
+  			<a class="carousel-control-next" href="#banner-images" data-slide="next">
+    			<span class="carousel-image-next"></span>
+  			</a>
+		</div-->	
+
+		
+		<!--Contents-->
+		<div class="container site-content">
+			<div class="row">
+				<div class="col-lg-8">
+					
+					
+					
+							</div>
+						</div>
+  						<div class="tab-pane container" id="content2">
+  							<div class="container-fluid">
+								<div class="row">
+									<div class="col-md-4 content-item">
+										<div class="card">
+  											<img class="card-img-top" src="<?php echo base_url(); ?>assets/images/img-not-available.jpg">
+											<div class="card-body">
+												<a href="#">
+													<h4 class="card-title">Room Name</h4>
+												</a>
+												<h6 class="card-text">Description</h6>
+								    			<span class="card-text old-price">Rp 199.999</span><br>
+								    			
+								    			<a href="#" class="btn btn-block">BOOKING NOW</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4 content-item">
+										<div class="card">
+  											<img class="card-img-top" src="<?php echo base_url(); ?>assets/images/img-not-available.jpg">
+											<div class="card-body">
+												<a href="#">
+													<h4 class="card-title">Room Name</h4>
+												</a>
+												<h6 class="card-text">Description</h6>
+								    			<span class="card-text old-price">Rp 199.999</span><br>
+								    			
+								    			<a href="#" class="btn btn-block">BOOKING NOW</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4 content-item">
+										<div class="card">
+  											<img class="card-img-top" src="<?php echo base_url(); ?>assets/images/img-not-available.jpg">
+											<div class="card-body">
+												<a href="#">
+													<h4 class="card-title">Room Name</h4>
+												</a>
+												<h6 class="card-text">Description</h6>
+								    			<span class="card-text old-price">Rp 199.999</span><br>
+								    			
+								    			<a href="#" class="btn btn-block">BOOKING NOW</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-4 content-item">
+										<div class="card">
+  											<img class="card-img-top" src="<?php echo base_url(); ?>assets/images/img-not-available.jpg">
+											<div class="card-body">
+												<a href="#">
+													<h4 class="card-title">Room Name</h4>
+												</a>
+												<h6 class="card-text">Description</h6>
+								    			<span class="card-text old-price">Rp 199.999</span><br>
+								    		
+								    			<a href="#" class="btn btn-block">BOOKING NOW</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4 content-item">
+										<div class="card">
+  											<img class="card-img-top" src="<?php echo base_url(); ?>assets/images/img-not-available.jpg">
+											<div class="card-body">
+												<a href="#">
+													<h4 class="card-title">Room Name</h4>
+												</a>
+												<h6 class="card-text">Description</h6>
+								    			<span class="card-text old-price">Rp 199.999</span><br>
+								    			
+								    			<a href="#" class="btn btn-block">BOOKING NOW</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4 content-item">
+										<div class="card">
+  											<img class="card-img-top" src="<?php echo base_url(); ?>assets/images/img-not-available.jpg">
+											<div class="card-body">
+												<a href="#">
+													<h4 class="card-title">Room Name</h4>
+												</a>
+												<h6 class="card-text">Description</h6>
+								    			<span class="card-text old-price">Rp 199.999</span><br>
+								    	
+								    			<a href="#" class="btn btn-block">BOOKING NOW</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-4 content-item">
+										
+									</div>
+									<div class="col-md-4 content-item">
+									</div>
+									<div class="col-md-4 content-item">
+									</div>
+								</div>
+							</div>
+  						</div>
+  						<div class="tab-pane container" id="content3">
+  							
+  						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+
+		<!--Footer-->
+		<div class="container-fluid bg-dark" id="main-footer">
+			<div class="row mx-5 py-3">
+				<div class="col-xs-4 w-50 text-right pr-3">
+					<a href="#">
+						About us
+					</a>
+				</div>
+				<div class="col-xs-6 w-50">
+					&copy; HotelU 2018. All rights reserved.
+				</div>
+			</div>
+		</div>
+<?php
+$this->load->view('footer');
+?>
