7ad97ae19b5aa18f938076ef5d4340099c3f2342
tiravieri <43974554+tiravieri@users.noreply.github.com>
2018-10-09 20:04:13 +0700

    Add files via upload

---
+++ application/views/Login.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/Login.css">
+</head>
+
+<body>
+	
+	<div class="container ">
+		<img class="coverBg" src="backhotel.png">	
+	</div>
+	
+	<div class="container">
+		
+		<h3 class="text-center">Login To HotelU</h3>
+		<div class="col-md-5 mx-auto">
+			<div class="form-group">
+				<label for="username">Email</label>
+			   <center><input class="form-control" type="text" name="username" placeholder="Email" required/></center> 
+			  </div>
+       		<div class="form-group">
+				<label for="password">Password</label>
+				<input class="form-control" type="password" name="password" placeholder="Password" />
+        	</div>
+			
+			<center><button type="submit" class="btn btn-lg btn-info">Login</button></center>
+			
+			<p>Belum Punya Akun? 
+           		<a href="#">Daftar disini.</a>
+          	</p>
+			
+			</form>
+		</div>
+		
+	</div>
+	
+	<footer>
+		
+		<div class="container footer">
+			<div class="row">
+				<div class="col-sm-3 atas">
+					<h5>About HotelU</h5>
+					<p class="about">Aplikasi ini merupakan aplikasi berbasis web.</p>
+            	</div>
+				
+				<div class="col-sm-2 atas">
+					<h5>Contact Us</h5>
+					<p class="about">Jalan Soekarno Hatta No.126 ,<br>Bandung, Jawa Barat <br> 022 - 7621897 <br>HotelU@gmail.com </p>
+            	</div>
+				
+				<div class="col-sm-2 atas">
+					<center><h5>Follow Us</h5></center>
+					
+					<a href="http://facebook.com/">
+						<img src="facebook.png" class="logoFol" alt="Fb">
+					</a>
+					
+					<a href="http://facebook.com/">
+						<img src="instagram.png" class="logoFol" alt="Ig">
+					</a>
+					
+					<a href="http://facebook.com/">
+						<img src="twitter.png" class="logoFol" alt="Tw">
+					</a>
+					
+            	</div>
+				
+				<div class="col-sm-2 atas">
+					<center><h5>Payment</h5></center>
+					
+					<img src="if_BCA_2425807.png" class="logoPay" alt="BCA">
+					<img src="if_BNI_2425812.png" class="logoPay" alt="BCA">
+					<img src="if_BRI_2425806.png" class="logoPay" alt="BCA">
+					<img src="if_Mandiri_2425804.png" class="logoPay"alt="BCA">
+					
+            	</div>
+			</div>
+		</div>
+	
+	</footer>
+	
+</body>
+</html>
