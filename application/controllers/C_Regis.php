b95756ebbfebd06f9123846eb319cfe5d5eb8af3
tiravieri <43974554+tiravieri@users.noreply.github.com>
2018-11-12 22:39:12 +0700

    Add files via upload

---
+++ application/controllers/C_Regis.php
@@ -0,0 +1,35 @@
+<?php
+
+	class C_Regis extends CI_Controller {
+		public function __construct(){
+			parent:: __construct();
+			$this->load->model('M_Regis');
+		}
+		
+		public function index(){
+			$this->load->view('Register');
+		}
+		
+		public function register(){
+			$user = $this->input->post('username');
+			$email = $this->input->post('email');
+			$pass = $this->input->post('password');
+			$nama = $this->input->post('nama');
+			$jk = $this->input->post('jk');
+			$umur = $this->input->post('umur');
+			
+			$data = array(
+				"username"=>$user,
+				"email"=>$email,
+				"password"=>$pass,
+				"nama"=>$nama,
+				"jk"=>$jk,
+				"umur"=>$umur
+			);
+			
+			$this->M_Regis->insert($data,'customer');
+			redirect('C_Akun/index');
+		}
+	}
+
+?>
