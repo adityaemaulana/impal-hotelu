ac93746b32cf2fd217e25c1d716ab20068aec751
tiravieri <43974554+tiravieri@users.noreply.github.com>
2018-11-16 15:41:49 +0700

    Update C_Akun.php

--- application/controllers/C_Akun.php
+++ application/controllers/C_Akun.php
@@ -19,16 +19,16 @@
 				$login = $this->M_Login->check($data);
 				if ($login){
 					$this->session->set_userdata('email',$login->email);
-					redirect('C_Akun/index')
+					redirect('C_Akun/index');
 				} else{
 					$this->session->set_flashdata('message','Error Login');
-					redirect('C_Akun/index')
+					redirect('C_Akun/index');
 				}
 			}
 			
 			public function logout(){
 				$this->session->sess_destroy();
-				redirect('C_Akun/index')
+				redirect('C_Akun/index');
 			}
 			
 	}	
